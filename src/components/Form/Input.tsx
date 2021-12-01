086472bfb9e2e0bb36ed9531378f18959b86089c
Lucas Passos <lucasplpx@gmail.com>
2021-11-30 21:40:05 -0300

    pagina SignIn

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,32 @@
+import {
+  Input as ChakraInput,
+  FormControl,
+  FormLabel,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor='pink.500'
+        bg='gray.900'
+        variant='filled'
+        _hover={{
+          bgColor: 'gray.900',
+        }}
+        size='lg'
+        {...rest}
+      />
+    </FormControl>
+  );
+}
