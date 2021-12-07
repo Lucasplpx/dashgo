4dc67bf4fde3615c62add2a7644a96c411cbce53
Lucas Passos <lucasplpx@gmail.com>
2021-12-07 20:46:37 -0300

    header responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,15 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
 import { Logo } from './Logo';
 import { NotificationsNav } from './NotificationsNav';
 import { Profile } from './Profile';
 import { SearchBox } from './SearchBox';
 
 export function Header() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
     <Flex
       as='header'
@@ -17,11 +22,12 @@ export function Header() {
       px='6'
     >
       <Logo />
-      <SearchBox />
+
+      {isWideVersion && <SearchBox />}
 
       <Flex align='center' ml='auto'>
         <NotificationsNav />
-        <Profile />
+        <Profile showProfileData={isWideVersion} />
       </Flex>
     </Flex>
   );
