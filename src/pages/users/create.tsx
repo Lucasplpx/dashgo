73928e69db81fd76131a8abb6b8b839acd62c389
Lucas Passos <lucasplpx@gmail.com>
2021-12-07 21:36:32 -0300

    fluxo de navegacao

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -8,6 +8,7 @@ import {
   HStack,
   Button,
 } from '@chakra-ui/react';
+import Link from 'next/link';
 import { Input } from '../../components/Form/Input';
 
 import { Header } from '../../components/Header';
@@ -21,18 +22,18 @@ export default function CreateUser() {
       <Flex w='100%' my='6' maxWidth={1480} mx='auto' px='6'>
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
+        <Box flex='1' borderRadius={8} bg='gray.800' p={['4', '8']}>
           <Heading size='lg' fontWeight='normal'>
             Criar usuário
           </Heading>
           <Divider my='6' borderColor='gray.700' />
 
           <VStack spacing='8'>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={['4', '8']} w='100%'>
               <Input name='name' label='Nome completo' />
               <Input name='email' type='email' label='E-mail' />
             </SimpleGrid>
-            <SimpleGrid minChildWidth='240px' spacing='8' w='100%'>
+            <SimpleGrid minChildWidth='240px' spacing={['4', '8']} w='100%'>
               <Input name='password' type='password' label='Senha' />
               <Input
                 name='password_confirmation'
@@ -43,8 +44,12 @@ export default function CreateUser() {
           </VStack>
 
           <Flex mt='8' justify='flex-end'>
-            <HStack spacing='4'>
-              <Button colorScheme='whiteAlpha'>Cancelar</Button>
+            <HStack spacing={['4', '8']}>
+              <Link href='/users' passHref>
+                <Button as='a' colorScheme='whiteAlpha'>
+                  Cancelar
+                </Button>
+              </Link>
               <Button colorScheme='pink'>Salvar</Button>
             </HStack>
           </Flex>
