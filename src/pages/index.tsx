527fce830ca2a6c224e8c1b12f0606441179916b
Lucas Passos <lucasplpx@gmail.com>
2021-12-10 21:33:46 -0300

    validacao dos dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import { Flex, Button, Stack } from '@chakra-ui/react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup/dist/yup';
 import { Input } from '../components/Form/Input';
 
 type SignInFormData = {
@@ -7,8 +9,16 @@ type SignInFormData = {
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória'),
+});
+
 export default function Home() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+  const { errors } = formState;
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise((resolve) => setTimeout(resolve, 2000));
@@ -32,12 +42,14 @@ export default function Home() {
             type='email'
             name='email'
             label='E-mail'
+            error={errors.email}
             {...register('email')}
           />
           <Input
             type='password'
             name='password'
             label='Senha'
+            error={errors.password}
             {...register('password')}
           />
         </Stack>
