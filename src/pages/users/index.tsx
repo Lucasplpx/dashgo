2092e9d69268de8cbcbb2445ac008001c1096066
Lucas Passos <lucasplpx@gmail.com>
2022-01-03 19:20:00 -0300

    sinalizando refetch e devtools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,24 +25,30 @@ import Link from 'next/link';
 import { useEffect } from 'react';
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery('users', async () => {
-    const response = await fetch('http://localhost:3000/api/users');
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery(
+    'users',
+    async () => {
+      const response = await fetch('http://localhost:3000/api/users');
+      const data = await response.json();
 
-    const users = data.users.map((user) => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }),
-      };
-    });
-    return users;
-  });
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+            day: '2-digit',
+            month: 'long',
+            year: 'numeric',
+          }),
+        };
+      });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -60,6 +66,9 @@ export default function UserList() {
           <Flex mb='8' justify='space-between' align='center'>
             <Heading size='lg' fontWeight='normal'>
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size='sm' color='gray.500' ml='4' />
+              )}
             </Heading>
             <Link href='/users/create' passHref>
               <Button
